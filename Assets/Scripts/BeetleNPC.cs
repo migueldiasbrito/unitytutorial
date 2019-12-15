f0a30653361e6f5faa247d800fca3e6c01ef365a
migueldiasbrito <migueldiasbrito@gmail.com>
2019-12-15 22:18:02 +0000

    Add Beetle NPC

---
+++ Assets/Scripts/BeetleNPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeetleNPC : MonoBehaviour
+{
+	Animator animator;
+	// Start is called before the first frame update
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Collision Detection Test
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.CompareTag("Player"))
+		{
+			animator.Play("Die on ground");
+		}
+	}
+}
